7ecf0ffead43af53f815663a8bf137375db90447
conservativ007 <lexxar_89@mail.ru>
2020-05-10 10:38:30 +0300

    добавил описание задач

--- web.php
+++ web.php
@@ -11,31 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return '!!!';
-});
 
-Route::get('/employee/{id}', 'EmployeeController@showOne');
-
-Route::get('/employee/{id}/{param}', 'EmployeeController@showField')
--> where([
-  'id' => '[0-9]+',
-  'param' => '(name|surname|salary)'
-]);
-
-  Route::get('test/show', 'TestController@show');
-  Route::get('derectiv/test', 'DerectivController@one');
-  Route::get('nasledovanie/test', 'NasledController@one');
-
-  Route::get('blade', function () {
-    return view('child');
-  });
-
-  Route::get('nasledtest', 'NasledController@one');
-  Route::get('practic/templ', 'TemplateController@test');
-
-  Route::get('/posts/{id}', 'PostController@showOne');
-  Route::get('/posts', 'PostController@showAll');
 
   Route::get('/product/{category_id}/{product_id}', 'ProductController@showProduct');
   Route::get('/product/{category_id}', 'ProductController@showCategory');
